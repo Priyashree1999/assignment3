78753fbba2bdb940d9adbcfab334619c81622178
PRIYASHREE ROY <101980665+Priyashree1999@users.noreply.github.com>
2022-03-28 12:41:36 -0500

    Add files via upload

---
+++ assignment3.1.py
@@ -0,0 +1,17 @@
+
+def sum_of_the_numbers(b):
+    sum=0
+    for i in b: 
+   
+        
+        sum=sum+i
+    return sum
+        
+    
+
+a=sum_of_the_numbers((8,2,3,0,7))
+print("Expected output: ",a)
+   
+
+    
+
